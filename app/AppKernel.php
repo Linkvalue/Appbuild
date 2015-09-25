b1b4b63edfdb2dff0b62d8a9eb06bab044eeeafb
Oliboy50 <oliver.thebault@gmail.com>
2015-09-25 18:12:21 +0200

    bootstrap project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,15 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // vendors
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Hautelook\AliceBundle\HautelookAliceBundle(),
+            new Majora\Bundle\FrameworkExtraBundle\MajoraFrameworkExtraBundle(),
+
+            // projects
             new AppBundle\AppBundle(),
         );
 
@@ -24,6 +33,8 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+//            $bundles[] = new Majora\GeneratorBundle\MajoraGeneratorBundle();
         }
 
         return $bundles;
