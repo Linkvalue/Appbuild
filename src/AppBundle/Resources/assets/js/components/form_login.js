5bae14bc633504c27ee01e56e9eb36b865802354
CeliaLfd <clia.lafond@gmail.com>
2017-02-14 11:23:30 +0100

    update script form-login

--- src/AppBundle/Resources/assets/js/components/form_login.js
+++ src/AppBundle/Resources/assets/js/components/form_login.js
@@ -1,42 +1,34 @@
-$('[data-is="form-login"]').on('change', function() {
-   animationInput(this);
+$('[data-is="form-login"]').on('change', (e) => {
+  animationInput(e.target);
  });
 
 function animationInput(form) {
-  var $form = $(form);
 
-   $form.each(function() {
+    var $formInput = $(form);
 
-     $(this).on('change', function() {
-       var $formInput = $(this).find('.input_login');
+    if ($formInput.val().length != 0) {
+      $formInput.addClass('active');
+    } else {
+      $formInput.removeClass('active');
+    };
 
-       $formInput.each(function() {
-         var $input = $(this);
-         if($input.val().length != 0) {
-           $input.addClass('active');
-         } else {
-           $input.removeClass('active');
-         }
-       });
-     });
-   });
 };
 
 
 // animation on login button if form is valid
 // TODO : check if the form is valid
 
-const $buttonLogin = $('[data-button-login]');
-
-if ($buttonLogin.length > 0) {
-  const buttonPos = $buttonLogin.offset();
-  const $loader = $('<div class="loader-login"></div>');
-
-  $loader.css('top', buttonPos.top + 'px');
-
-  $buttonLogin.after($loader);
-
-  $buttonLogin.click((e) => {
-    $loader.addClass('valid');
-  });
-}
+// const $buttonLogin = $('[data-button-login]');
+//
+// if ($buttonLogin.length > 0) {
+//   const buttonPos = $buttonLogin.offset();
+//   const $loader = $('<div class="loader-login"></div>');
+//
+//   $loader.css('top', buttonPos.top + 'px');
+//
+//   $buttonLogin.after($loader);
+//
+//   $buttonLogin.click((e) => {
+//     $loader.addClass('valid');
+//   });
+// }
